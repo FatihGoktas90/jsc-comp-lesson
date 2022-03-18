93857f4fd0455f3f652c3074b21081cbf1ca2653
Fatih <goktasfatih90@gmail.com>
2022-03-18 16:01:36 +0300

    updated

--- src/components/ClassComp.js
+++ src/components/ClassComp.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import FuncComp from './FuncComp';
 class ClassComp extends React.Component{
-    
+    constructor(props){
+        super(props);
+        this.state={student :"Fatih", counter:1}
+        // state ={student:"Fatih", counter:1}
+    }
+   changeCounter= ()=> {
+       this.setState({counter:this.state.counter+1})
+   }
+   decreaseCounter = ()=> {
+       this.setState({counter:this.state.counter-1})
+   }
     render(){
+        const {old,car}=this.props;
         
         return (
             <div>
              <h1>Welcome to ReactJs from CC</h1>
-            <p>Age : {this.props.old}</p>
-            <p>Car : {this.props.car}</p>
-            
+            <p>Age : {old}</p>
+            <p>Car : {car}</p>
+            <p>Student :{this.state.student}</p>
+            <p>Counter : {this.state.counter}</p>
+            <button onClick={this.changeCounter}>Increase</button>
+            <button onClick={this.decreaseCounter}>Decrease</button>
             </div>
         )
 
--- src/components/FuncComp.js
+++ src/components/FuncComp.js
@@ -4,9 +4,9 @@ function FuncComp({ old,color }){
     // const { old } = props;
     return (
     <>
-    <h1>Welcome to ReactJS from FC</h1>
-    <p>Age : {old}</p>
-    <p>Color : {color.toUpperCase()}</p>
+         <h1>Welcome to ReactJS from FC</h1>
+         <p>Age : {old}</p>
+         <p>Color : {color.toUpperCase()}</p>
     
     </>
     )
