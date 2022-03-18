88e39c9cc600f649e941c4cc98c5b8f02fdccca6
Fatih <goktasfatih90@gmail.com>
2022-03-18 19:17:16 +0300

    updated

--- src/components/FuncComp.js
+++ src/components/FuncComp.js
@@ -1,12 +1,24 @@
 import React from 'react'
+import {useState} from 'react'
 // function FuncComp(props){
 function FuncComp({ old,color }){
     // const { old } = props;
+    const [counter,setCounter]=useState(0);
+    const incCounter = ()=>{
+        setCounter(counter+1);
+    }
+    const decCounter = ()=>{
+        setCounter(counter-1);
+    }
+    
     return (
     <>
          <h1>Welcome to ReactJS from FC</h1>
          {/* <p>Age : {old}</p> */}
          {/* <p>Color : {color.toUpperCase()}</p> */}
+         <p>{counter}</p>
+         <button onClick={incCounter}>Increase</button>
+         <button onClick={decCounter}>Decrease</button>
     
     </>
     )
